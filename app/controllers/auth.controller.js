3b963e0d702bd9b2b0882c9152cd088cb13dd399
thaipq <pqthai2502@gmail.com>
2023-11-14 23:06:51 +0700

    add routing to signing in

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,2 +1,22 @@
 const db = require("../models");
 const config = require("../config/auth.config");
+const jwt = require("jsonwebtoken");
+
+const signin = (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const accessToken = jwt.sign({ password }, config.secret, {
+    expiresIn: 3600 // 1 hour
+  });
+  res.json({
+    username: username,
+    accessToken: accessToken
+  });
+}
+
+const signup = (req, res) => { }
+
+module.exports = {
+    signin,
+    signup
+}
--- app/routes/auth.routers.js
+++ app/routes/auth.routers.js
@@ -1,5 +1,7 @@
 const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/auth.controller");
-module.exports = function(app) {
-    
-}
+const router = require("express").Router();
+
+router.post("/signin", controller.signin);
+
+module.exports = router
