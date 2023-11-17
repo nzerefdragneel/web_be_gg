6893cb41ab624b5157d6a44732ef4572df5d0a74
20120519 <lieunguyenthi1802@gmail.com>
2023-11-17 15:43:43 +0700

    sign-up

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "1",
+    USER: process.env.DB_USERNAME,
+    PASSWORD: process.env.DB_PASSWORD,
     DB: "dbtest",
     dialect: "postgres",
     pool: {
--- app/routes/auth.routers.js
+++ app/routes/auth.routers.js
@@ -1,5 +1,12 @@
 const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/auth.controller");
 module.exports = function(app) {
+    app.post(
+        "/api/auth/signup",
+        [
+          verifySignUp.checkDuplicateUsernameOrEmail,
+        ],
+        controller.signup
+      );
     
 }
