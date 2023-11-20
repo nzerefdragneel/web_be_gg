776614d500d53edc9601c931c9e30aa97e92fd34
nzerefdragneel <nguyentranngocsuong94@gmail.com>
2023-11-20 16:37:37 +0700

    connect public database

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,7 @@
 const config = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
+/*
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   host: config.HOST,
   dialect: config.dialect,
@@ -13,7 +14,9 @@ const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   define: {
     freezeTableName: true,
   },
-});
+  ssl:true
+});*/
+const sequelize = new Sequelize('postgres://username:eMb5kLAT4Z3Bz7ykYqmXSzAY8wxRJkZb@dpg-cldh45eg1b2c73f7b63g-a.oregon-postgres.render.com/db_f2lf?ssl=true');
 
 const db = {};
 
