7378d65eb0b67bfb2f97ca99356edf0b220f0a3f
nzerefdragneel <nguyentranngocsuong94@gmail.com>
2023-11-20 16:03:35 +0700

    host db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,11 @@
+fs = require('fs');
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "1",
-    DB: "dbtest",
+    HOST: "dpg-cldh45eg1b2c73f7b63g-a.oregon-postgres.render.com",
+    USER: "username",
+    PASSWORD: "eMb5kLAT4Z3Bz7ykYqmXSzAY8wxRJkZb",
+    DB: "db_f2lf",
     dialect: "postgres",
+    ssl:true,
     pool: {
       max: 5,
       min: 0,
