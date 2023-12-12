6d2dd716a89ae111753fbafb9662ec002e465d9b
Ngọc Sương <75318797+nzerefdragneel@users.noreply.github.com>
2023-12-12 12:22:24 +0700

    Update errollment.model.js

--- app/models/errollment.model.js
+++ app/models/errollment.model.js
@@ -1,15 +1,12 @@
 module.exports = (sequelize, Sequelize) => {
     const Enrollment = sequelize.define("enrollments", {
-      enrollmentId: {
+      studentId: {
         type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+        primaryKey: true
       },
       classId: {
-        type: Sequelize.INTEGER
-      },
-      studentId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        primaryKey: true
       },
       enrollmentDate: {
         type: Sequelize.DATE
